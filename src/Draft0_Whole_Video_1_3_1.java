a3356a7f0f7c8439d71e599e25108b0f3529f0aa
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-29 12:54:11 +0300

    Initial commit

--- src/Draft0_Whole_Video_1_3_1.java
+++ src/Draft0_Whole_Video_1_3_1.java
@@ -1,5 +1,6 @@
-public class Main {
+public class Draft0_Whole_Video_1_3_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание : \n1.\tСоздайте классы с полями для описания структуры университета." +
+                "\n\nРешение: ");
     }
 }
--- src/Draft1_Task_3_1.java
+++ src/Draft1_Task_3_1.java
@@ -1,4 +1,4 @@
-public class Draft0_Whole_Video_1_3_1 {
+public class Draft1_Task_3_1 {
     public static void main(String[] args) {
         System.out.println("Задание : \n1.\tСоздайте классы с полями для описания структуры университета." +
                 "\n\nРешение: ");
