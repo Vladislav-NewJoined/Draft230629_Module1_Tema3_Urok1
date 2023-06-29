2a497c7a1986e05b51273013d79403b1c6398671
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-29 12:52:41 +0300

    Initial commit

---
+++ src/Draft1_Task_3_1.java
@@ -0,0 +1,6 @@
+public class Draft0_Whole_Video_1_3_1 {
+    public static void main(String[] args) {
+        System.out.println("Задание : \n1.\tСоздайте классы с полями для описания структуры университета." +
+                "\n\nРешение: ");
+    }
+}
