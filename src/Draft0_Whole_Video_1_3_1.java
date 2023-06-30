ea3eec726a30574ad7fdaf6a708875e297420c94
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-30 09:46:56 +0300

    Initial commit

--- src/Draft0_Whole_Video_1_3_1.java
+++ src/Draft0_Whole_Video_1_3_1.java
@@ -2,5 +2,23 @@ public class Draft0_Whole_Video_1_3_1 {
     public static void main(String[] args) {
         System.out.println("Задание : \n1.\tСоздайте классы с полями для описания структуры университета." +
                 "\n\nРешение: ");
+// ООП ( Объектно-ориентированное программирование ) - это всего лишь способ организовать
+// код, сделать его не одной подряд просстынёй, подряд идущей, а разбить его на разные
+// кусочки.
+
+// сколько экземпляров класса у нас будет (Котов, Файлов, итд) - столько роз надо написать new
+// Класс - описание структуры, например - какие есть поля.
+// Экземпляр класса - уже реальный объект, который имеет значения (имя, возраст итд)
+// Инстанс класса (closs instance) - синоним экзмепляру класса
+
+//        видео мин 50 05
+//        for (int i = 0; i < filelnfos.length; j++) {
+//            System.out.println("file " + i + " + fileInfos[i].fileName);
+//                    Filelnfornation currentlnfo = filelnfos[i];
+//            long currentFileSize = currentlnfo.size;
+//            if (currentFileSize > maxSize) {
+//                maxSize = currentFileSize;
+//            }
+//        }
     }
 }
