ff54a24ea8868f661e53dac7f9f557ebd2d8fa89
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-30 19:04:05 +0300

    Initial commit

--- src/Draft1_Task_3_1.java
+++ src/Draft1_Task_3_1.java
@@ -2,87 +2,30 @@ import java.io.File;
 
 public class Draft1_Task_3_1 {
 
-//        Пример _ ППППППППППППППППППППППППППППППППППП
+    //        Пример _ ППППППППППППППППППППППППППППППППППП
     public static void main(String[] args) {
         System.out.println("Задание : \n1.\tСоздайте классы с полями для описания структуры университета." +
                 "\n\nРешение: ");
+        System.out.println("Создаем класс UniversityStructure со следующими полями: Факуьтет, Срок обучения (лет)");
 
+        UniversityStructure universityStructure1 = new UniversityStructure();
+        universityStructure1.faculty = "Юриспруденция";
+        universityStructure1.studyPeriod = 3;
+        UniversityStructure universityStructure2 = new UniversityStructure();
+        universityStructure2.faculty = "Программирование";
+        universityStructure2.studyPeriod = 4;
 
-
-
-
-
-
-
-
-
-            File file = new File("test.txt");
-            /*String absolutePath = file.getAbsolutePath();
-            String fileName = file.getName();
-            long size = file.length();*/
-
-            FileInformation info = new FileInformation();
-            info.absolutePath = file.getAbsolutePath();
-            info.fileName = file.getName();
-            info.size = file.length();
-
-            File file2 = new File("book.txt");
-            FileInformation info2 = new FileInformation();
-            info2.absolutePath = file2.getAbsolutePath();
-            info2.fileName = file2.getName();
-            info2.size = file2.length();
-
-
-//            if (size>size2) {
-//                System.out.println("Bigger file : ");
-//                System.out.println(fileName);
-//                System.out.println("Path: " + absolutePath);
-//                System.out.println("Size is: " + size);
+//        String faculty1 = "Юриспруденция";
+//        int studyPeriod1 = 3;
+//        String faculty2 = "Программирование";
+//        int studyPeriod2 = 4;
+//        String faculty3 = "Психология";
+//        int studyPeriod3 = 5;
 //
-//
-//            } else {
-//                System.out.println("Bigger file : ");
-//                System.out.println(fileName2);
-//                System.out.println("Path: " + absolutePath2);
-//                System.out.println("Size is: " + size2);
-//
-//
-//            }
-//            FileInformation bigInfo = info.size > info2.size ? info : info2;
-//            if (info.size>info2.size) {
-//                bigInfo = info;
-//                /*System.out.println("Bigger file : ");
-//                System.out.println(info.fileName);
-//                System.out.println("Path: " + info.absolutePath);
-//                System.out.println("Size is: " + info.size);*/
-//
-//
-//            } else {
-//                bigInfo = info2;
-//                /*System.out.println("Bigger file : ");
-//                System.out.println(info2.fileName);
-//                System.out.println("Path: " + info2.absolutePath);
-//                System.out.println("Size is: " + info2.size);*/
-//            }
-
-            //  Заменить на тернарный оператор
-        FileInformation bigInfo = info.size > info2.size ? info : info2;
-
-
-        System.out.println("Bigger file : ");
-                System.out.println(bigInfo.fileName);
-                System.out.println("Path: " + bigInfo.absolutePath);
-                System.out.println("Size is: " + bigInfo.size);
-
-
-
+//        System.out.println("Факультет: " + faculty3 + ", Срок обучения: " + studyPeriod3 + " лет.");
     }
 }
 //        Конец Примера _ КККККККККККККККК
-
-
-
-
 
 
 ////        Пример 2 ППППППППППППППППППППППППППППППППППП
@@ -153,10 +96,6 @@ public class Draft1_Task_3_1 {
 //    }
 //}
 ////        Конец Примера 2 КККККККККККККККК
-
-
-
-
 
 
 ////        Пример 1 ППППППППППППППППППППППППППППППППППП
