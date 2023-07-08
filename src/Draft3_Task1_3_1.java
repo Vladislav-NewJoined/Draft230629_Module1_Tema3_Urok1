cd09eb9d3ee552a74073eb47918494b11343ac34
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-08 14:49:05 +0300

    Initial commit

--- src/Draft3_Task1_3_1.java
+++ src/Draft3_Task1_3_1.java
@@ -9,65 +9,111 @@ public class Draft3_Task1_3_1 {
                 "(типизации, версии, массив ключевых слов..)\n\nРешение: ");
         // Закончил на видео мин 34 25
 
-//        String name1 = "Barsik";
-//        int age1 = 4;
-//        String name2 = "Murzik";
-//        int age2 = 13;
-//        String name3 = "Chernish";
-//        int age3 = 1;
 
-        Cat cat1 = new Cat();
-        cat1.name = "Barsik";
-        cat1.age = 4;
-        Cat cat2 = new Cat();
-        cat2.name = "Murzik";
-        cat2.age = 13;
-        Cat cat3 = new Cat();
-        cat3.name = "Chernish";
-        cat3.age = 1;
+    // Создаем массив с ключевыми словами (KeyWords).
+        System.out.println("Создаем класс для ключевых слов (KeyWords) с переменными и массивом.");
+        KeyWords keyWords1 = new KeyWords();
+        keyWords1.javaWord1 = "class";
+        keyWords1.javaWord2 = "int";
+        keyWords1.javaWord3 = "return";
 
-        System.out.println("Meow! Im  " + cat3.name + ", " +  cat3.age + " years old");
-        System.out.println(); // перенос строки
+        KeyWords keyWords2 = new KeyWords();
+        keyWords2.pythonWord1 = "global";
+        keyWords2.pythonWord2 = "pass";
+        keyWords2.pythonWord3 = "elif";
 
+        KeyWords keyWords3 = new KeyWords();
+        keyWords3.visualBasic1 = "Get";
+        keyWords3.visualBasic2 = "Next";
+        keyWords3.visualBasic3 = "ParamArray";
 
+        /*System.out.println(keyWords3.visualBasic1 + ", " + keyWords3.visualBasic2 + ", " + keyWords3.visualBasic3);*/
 
+        /*String[] wordsArray = {cat4.javaWord1, cat4.javaWord2, cat4.javaWord3};
+        System.out.print("Печатаем массив: " + Arrays.toString(wordsArray));*/
 
-    // Массив с ключевыми словами
-//        String javaWord1 = "javaWord1";
-//        String javaWord2 = "javaWord2";
-//        String javaWord3 = "javaWord3";
-
-        Cat cat4 = new Cat();
-        cat4.javaWord1 = "javaWord1";
-        cat4.javaWord2 = "javaWord2";
-        cat4.javaWord3 = "javaWord3";
-        Cat cat5 = new Cat();
-        cat5.pythonWord1 = "pythonWord1";
-        cat5.pythonWord2 = "pythonWord1";
-        cat5.pythonWord3 = "pythonWord1";
-
-        System.out.println(cat4.javaWord1 + ", " + cat4.javaWord2 + ", " + cat4.javaWord3 + "\n" + cat5.pythonWord1
-         + ", " + cat5.pythonWord2 + ", " + cat5.pythonWord3);
-
-        String[] wordsArray = {cat4.javaWord1, cat4.javaWord2, cat4.javaWord3};
-        System.out.print("Печатаем массив: " + Arrays.toString(wordsArray));
-
-        Cat cat6 = new Cat();
-        cat6.array2 = new String[]{cat5.pythonWord1, cat5.pythonWord2, cat5.pythonWord3};
+        KeyWords keyWords4 = new KeyWords();
+        KeyWords keyWords5 = new KeyWords();
+        KeyWords keyWords6 = new KeyWords();
+        keyWords4.array = new String[]{keyWords1.javaWord1, keyWords1.javaWord2, keyWords1.javaWord3};
+        keyWords5.array = new String[]{keyWords2.pythonWord1, keyWords2.pythonWord2, keyWords2.pythonWord3};
+        keyWords6.array = new String[]{keyWords3.visualBasic1, keyWords3.visualBasic2, keyWords3.visualBasic3};
+        System.out.println("Печатаем элементы массивов ключевых слов:");
+        System.out.print("Ключевые слова для Java: " + Arrays.toString(keyWords4.array) + "\n" +
+                         "Ключевые слова для Python: " + Arrays.toString(keyWords5.array) + "\n" +
+                         "Ключевые слова для Visual Basic: " + Arrays.toString(keyWords6.array));
         System.out.println(); //  перенос строки
-        System.out.print("Ещё печатаем массив: " + Arrays.toString(cat6.array2));
-
-
-
-
-
-
-
-
 
     }
 }
 ////        Конец Примера _ КККККККККККККККК
+
+
+
+
+
+
+
+
+
+////        Пример 5 ППППППППППППППППППППППППППППППППППП
+//public class Draft3_Task1_3_1 {
+//    public static void main(String[] args) {
+//        System.out.println("Задание 4.: \n1.\tСоздайте классы для описания языков программирования " +
+//                "(типизации, версии, массив ключевых слов..)\n\nРешение: ");
+//        // Закончил на видео мин 34 25
+//
+////        String name1 = "Barsik";
+////        int age1 = 4;
+////        String name2 = "Murzik";
+////        int age2 = 13;
+////        String name3 = "Chernish";
+////        int age3 = 1;
+//
+//        Cat cat1 = new Cat();
+//        cat1.name = "Barsik";
+//        cat1.age = 4;
+//        Cat cat2 = new Cat();
+//        cat2.name = "Murzik";
+//        cat2.age = 13;
+//        Cat cat3 = new Cat();
+//        cat3.name = "Chernish";
+//        cat3.age = 1;
+//
+//        System.out.println("Meow! Im  " + cat3.name + ", " +  cat3.age + " years old");
+//        System.out.println(); // перенос строки
+//
+//
+//
+//
+//    // Массив с ключевыми словами
+////        String javaWord1 = "javaWord1";
+////        String javaWord2 = "javaWord2";
+////        String javaWord3 = "javaWord3";
+//
+//        Cat cat4 = new Cat();
+//        cat4.javaWord1 = "javaWord1";
+//        cat4.javaWord2 = "javaWord2";
+//        cat4.javaWord3 = "javaWord3";
+//        Cat cat5 = new Cat();
+//        cat5.pythonWord1 = "pythonWord1";
+//        cat5.pythonWord2 = "pythonWord1";
+//        cat5.pythonWord3 = "pythonWord1";
+//
+//        System.out.println(cat4.javaWord1 + ", " + cat4.javaWord2 + ", " + cat4.javaWord3 + "\n" + cat5.pythonWord1
+//         + ", " + cat5.pythonWord2 + ", " + cat5.pythonWord3);
+//
+//        String[] wordsArray = {cat4.javaWord1, cat4.javaWord2, cat4.javaWord3};
+//        System.out.print("Печатаем массив: " + Arrays.toString(wordsArray));
+//
+//        Cat cat6 = new Cat();
+//        cat6.array2 = new String[]{cat5.pythonWord1, cat5.pythonWord2, cat5.pythonWord3};
+//        System.out.println(); //  перенос строки
+//        System.out.print("Ещё печатаем массив: " + Arrays.toString(cat6.array2));
+//
+//    }
+//}
+//////        Конец Примера 5 КККККККККККККККК
 
 
 
@@ -85,8 +131,8 @@ public class Draft3_Task1_3_1 {
 //        System.out.println("Задание 4.: \n1.\tСоздайте классы для описания языков программирования " +
 //                "(типизации, версии, массив ключевых слов..)\n\nРешение: ");
 //
-////        System.out.println("Создаем класс WordsOfProgLanguages для описания языков.");
-////        WordsOfProgLanguages wordsOfProgLanguage = new WordsOfProgLanguages();
+////        System.out.println("Создаем класс KeyWords для описания языков.");
+////        KeyWords wordsOfProgLanguage = new KeyWords();
 ////        wordsOfProgLanguage.javaWord1 = "javaWord1";
 ////        wordsOfProgLanguage.javaWord2 = "javaWord2";
 ////        wordsOfProgLanguage.javaWord3 = "javaWord3";
@@ -97,7 +143,7 @@ public class Draft3_Task1_3_1 {
 ////        wordsOfProgLanguage.javaScriptWord2 = "javaScriptWord2";
 ////        wordsOfProgLanguage.javaScriptWord3 = "javaScriptWord3";
 ////
-////        /*WordsOfProgLanguages[] words = new WordsOfProgLanguages[] {"javaWord1", };*/
+////        /*KeyWords[] words = new KeyWords[] {"javaWord1", };*/
 //
 //    }
 //}
@@ -119,8 +165,8 @@ public class Draft3_Task1_3_1 {
 //        System.out.println("Задание 4.: \n1.\tСоздайте классы для описания языков программирования " +
 //                "(типизации, версии, массив ключевых слов..)\n\nРешение: ");
 //
-//        System.out.println("Создаем класс WordsOfProgLanguages для описания языков.");
-//        WordsOfProgLanguages wordsOfProgLanguage = new WordsOfProgLanguages();
+//        System.out.println("Создаем класс KeyWords для описания языков.");
+//        KeyWords wordsOfProgLanguage = new KeyWords();
 //        wordsOfProgLanguage.javaWord1 = "javaWord1";
 //        wordsOfProgLanguage.javaWord2 = "javaWord2";
 //        wordsOfProgLanguage.javaWord3 = "javaWord3";
@@ -131,7 +177,7 @@ public class Draft3_Task1_3_1 {
 //        wordsOfProgLanguage.javaScriptWord2 = "javaScriptWord2";
 //        wordsOfProgLanguage.javaScriptWord3 = "javaScriptWord3";
 //
-//        /*WordsOfProgLanguages[] words = new WordsOfProgLanguages[] {"javaWord1", };*/
+//        /*KeyWords[] words = new KeyWords[] {"javaWord1", };*/
 //
 //
 //    }
@@ -149,7 +195,7 @@ public class Draft3_Task1_3_1 {
 //        System.out.println("Задание 4.: \n1.\tСоздайте классы для описания языков программирования " +
 //                "(типизации, версии, массив ключевых слов..)\n\nРешение: ");
 //
-//        System.out.println("Создаем класс WordsOfProgLanguages для описания языков.");
+//        System.out.println("Создаем класс KeyWords для описания языков.");
 //
 //    }
 //}
